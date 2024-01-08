f507b830837ae05ceab5a9b952ebf6902383c93c
AritzMendive <amendivnic11@educacion.navarra.es>
2024-01-08 17:15:18 +0100

    Creación de la clase Equipo

---
+++ src/Equipo.java
@@ -0,0 +1,49 @@
+public class Equipo {
+    private String nombre;
+    private String ciudad;
+    private final int numMaxJugadores = 22;
+    private Jugador[] ListaJugadores;
+
+    public Equipo(String nombre, String ciudad){
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.ListaJugadores = new Jugador[numMaxJugadores];
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+    public int getNumJugadores(){
+        int hayJugador = 0;
+        for(int i = 0; i < ListaJugadores.length; i++){
+            if(ListaJugadores[i] != null){
+                hayJugador++;
+            }
+        }
+        return hayJugador;
+    }
+
+    public void adquirirJugador(Jugador nuevoJugador){
+        for(int i = 0; i< ListaJugadores.length; i++){
+            if (ListaJugadores[i] == null){
+                ListaJugadores[i] = nuevoJugador;
+                break;
+            }
+        }
+    }
+
+    public void venderJugador(String nombreJugador){
+        if (ListaJugadores != null){
+            for (int i = 0; i < ListaJugadores.length; i++){
+                if (ListaJugadores[i].getNombre() == nombreJugador){
+                    ListaJugadores[i] = null;
+                    break;
+                }
+            }
+        }
+    }
+}
